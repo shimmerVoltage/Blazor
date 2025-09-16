69f2d9da94674639096d2fd4ad9075c172a59926
shimmerVoltage <shimmerVoltage@SHIMMERVOLTAGE>
2025-09-16 21:37:49 +0300

    TODOlist in progress

---
+++ IntroductionToBlazor/Components/Pages/Factorial.razor.cs
@@ -0,0 +1,16 @@
+﻿namespace IntroductionToBlazor.Components.Pages
+{
+    public partial class Factorial
+    {
+        int n = 0;
+        long fact = 1;
+        void Calculate()
+        {
+            fact = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                fact *= i;
+            }
+        }
+    }
+}
