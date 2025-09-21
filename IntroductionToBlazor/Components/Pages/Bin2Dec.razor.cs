778fa3f157206cbb51c20add3ea0a862774295bb
shimmerVoltage <shimmerVoltage@SHIMMERVOLTAGE>
2025-09-21 14:25:05 +0300

    Homework DONE

---
+++ IntroductionToBlazor/Components/Pages/Bin2Dec.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace IntroductionToBlazor.Components.Pages
+{
+    public partial class Bin2Dec
+    {
+        int dec = 0;
+        string bin = "";
+        private void OnBinInput(ChangeEventArgs e)
+        {
+            string input = e.Value?.ToString() ?? "";
+            if (string.IsNullOrEmpty(input) || input.All(c => c == '0' || c == '1'))
+            {
+                bin = input;
+                Cunvert();
+            }
+        }
+        void Cunvert()
+        {
+            dec = bin
+            .Select((c, index) => new { Digit = c, Position = bin.Length - index - 1 })
+            .Where(x => x.Digit == '1')
+            .Sum(x => (int)Math.Pow(2, x.Position));
+        }
+    }
+}
---
+++ IntroductionToBlazor/Components/Pages/Dec2Bin.razor.cs
@@ -0,0 +1,14 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace IntroductionToBlazor.Components.Pages
+{
+    public partial class Dec2Bin
+    {
+        int dec;
+        string bin = "";
+        void Cunvert()
+        {
+            bin = Convert.ToString(dec, 2);
+        }
+    }
+}
