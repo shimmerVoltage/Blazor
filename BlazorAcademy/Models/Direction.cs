d07e15fce93075af3176ed9abdcd44348b8e35ff
shimmerVoltage <shimmerVoltage@SHIMMERVOLTAGE>
2025-09-23 21:40:38 +0300

    Blazor Academy in progress

---
+++ BlazorAcademy/Models/Direction.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlazorAcademy.Models
+{
+    public class Direction
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public byte direction_id { get; set; }
+        [Required]
+        public string direction_name { get; set; }
+        
+    }
+}
